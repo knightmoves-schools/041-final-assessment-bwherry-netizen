414b480be1fd21e176f1019e303eca9669704b94
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-13 15:47:21 +0000

    gg1.2

--- Lesson41/6InsertIngredients.sql
+++ Lesson41/6InsertIngredients.sql
@@ -1,1 +1,11 @@
---Add Your SQL Here
+INSERT INTO Ingredient (id, name)
+VALUES
+(1, 'Potatoes'),
+(2, 'Butter'),
+(3, 'Salt'),
+(4, 'Pepper'),
+(5, 'Bread(slice)'),
+(6, 'Egg'),
+(7, 'Milk'),
+(8, 'Vanilla Extract'),
+(9, 'Cinnamon');
--- Lesson41/7InsertMeasures.sql
+++ Lesson41/7InsertMeasures.sql
@@ -1,1 +1,8 @@
---Add Your SQL Here
+INSERT INTO Measure (id, name)
+VALUES
+(1, 'Milliliter'),
+(2, 'Tablespoon'),
+(3, 'Teaspoon'),
+(4, 'Pinch'),
+(5, 'Grams'),
+(6, 'Count');
