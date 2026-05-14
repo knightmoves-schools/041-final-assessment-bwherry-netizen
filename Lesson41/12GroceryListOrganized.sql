bf175e13ce03bbef19fb9978b6f19c00bb1866b6
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-14 13:56:22 +0000

    gg1.6

--- Lesson41/12GroceryListOrganized.sql
+++ Lesson41/12GroceryListOrganized.sql
@@ -1,1 +1,9 @@
---Add Your SQL Here
+SELECT DISTINCT
+    Ingredient.name AS "Ingredient Name",
+    Recipe.name AS "Recipe Name"
+FROM RecipeIngredient
+JOIN Ingredient
+    ON RecipeIngredient.ingredient_id = Ingredient.id
+JOIN Recipe
+    ON RecipeIngredient.recipe_id = Recipe.id
+ORDER BY Recipe.name ASC;
--- Lesson41/13CountingIngredients.sql
+++ Lesson41/13CountingIngredients.sql
@@ -1,1 +1,7 @@
---Add Your SQL Here
+SELECT
+    Recipe.name,
+    COUNT(RecipeIngredient.ingredient_id) AS ingredient_count
+FROM Recipe
+LEFT JOIN RecipeIngredient
+    ON Recipe.id = RecipeIngredient.recipe_id
+GROUP BY Recipe.id, Recipe.name;
