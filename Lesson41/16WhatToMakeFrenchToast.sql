c9d5ce1a36c216d3a3e7441f482d8c8b0794d217
Brendon W. <bwherry@midland.k12.ia.us>
2026-05-19 18:15:20 +0000

    gg2

--- Lesson41/16WhatToMakeFrenchToast.sql
+++ Lesson41/16WhatToMakeFrenchToast.sql
@@ -1,1 +1,10 @@
-
+SELECT Ingredient.name
+FROM RecipeIngredient
+JOIN Recipe
+    ON RecipeIngredient.recipe_id = Recipe.id
+JOIN Ingredient
+    ON RecipeIngredient.ingredient_id = Ingredient.id
+JOIN Pantry
+    ON Ingredient.id = Pantry.ingredient_id
+WHERE Recipe.name = 'French Toast'
+AND Pantry.in_stock = 'N';
